4fbee97aceda4813cd1077ffa95ab0b035a5b208
Glory Wong <glorywong1001@gmail.com>
2025-04-06 04:53:19 +0800

    feat: normalize cache dir && use goreleaser ci

--- utils/getCacheFilePath.ts
+++ utils/getCacheFilePath.ts
@@ -1,4 +1,7 @@
 import { crypto } from '@std/crypto/crypto'
+import envPaths from 'env-paths'
+import { join } from '@std/path'
+import { ensureDir } from '@std/fs'
 
 function buffer2hex(buffer: ArrayBuffer) { // buffer is an ArrayBuffer
   return [...new Uint8Array(buffer)]
@@ -7,8 +10,10 @@ function buffer2hex(buffer: ArrayBuffer) { // buffer is an ArrayBuffer
 }
 
 export async function getCacheFilePath(url: string) {
+  const cacheDir = envPaths('weather-cli').cache
+  await ensureDir(cacheDir)
   const hash = buffer2hex(
     await crypto.subtle.digest('SHA-256', new TextEncoder().encode(url)),
   )
-  return `./.cache_${hash}`
+  return join(cacheDir, `./.cache_${hash}`)
 }
